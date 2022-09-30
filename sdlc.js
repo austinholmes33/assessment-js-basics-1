53b34eb23b5c6189dbf998241b058b50ace11c0e
Austin Holmes <austin.holmes4@gmail.com>
2022-09-30 10:57:06 -0600

    sdlc complete

--- sdlc.js
+++ sdlc.js
@@ -19,8 +19,10 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
-const analysisOfRequirements = "The second step "
-const design = ""
-const implementation = ""
-const testingAndIntegration = ""
-const maintain = ""
+const analysisOfRequirements = "The second step involves understanding what is needed of the software, more particularly what is required of the devs to reach a certain goal with the software. This can include performing research of user needs, gathering specific details required of the system, and more."
+const design = "This is where the team will outline the initial outline, or design document, that will be referenced throughout the project, relating to the general details of the overall application."
+const implementation = "This is where the overall design established in the previous step comes together, or more simply, where the bulk of the coding takes place. The translation of the design into coding language."
+const testingAndIntegration = "Once the software is built, the fucntionality of it is tested to ensure that it works according to what was planned, designed, and expected of it."
+const maintain = "Once the software starts to actually be utilized, the product needs to be maintained in accordance with changing environments due to technology, updates, user desires, further bugs, etc."
+
+// As Jared mentioned in the first lecture, I am on the fence about the ordering of the first two steps, as I think the analysis of requirements can often come before the planning phase. I do not think there is any set hierarchy for these two, and the messiness of reality will probably often create situations where they blend and meld into each other. 
